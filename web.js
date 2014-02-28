1479d3d09b08ec1f1ec5d2ee069137822342c68f
andy <juliadi@femaledaily.com>
2014-02-28 15:09:22 +0700

    display couchdb data from cloudant

--- web.js
+++ web.js
@@ -1,11 +1,17 @@
 var express = require("express");
 var logfmt = require("logfmt");
+var restler = require('restler');
 var app = express();
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  restler.get('https://jeydeveloper.cloudant.com:443/_all_dbs', {
+	username: 'jeydeveloper',
+	password: 'okey19aja',
+  }).on('complete', function (data) {
+        res.send(data);
+  });
 });
 
 var port = Number(process.env.PORT || 5000);
